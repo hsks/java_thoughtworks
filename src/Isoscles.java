e787b7253fea15739b854cbe28dad6c852af61a9
Harsh <harshskulshrestha@gmail.com>
2016-08-13 23:51:49 +0530

    Isoscles Prgram Complete

--- src/Isoscles.java
+++ src/Isoscles.java
@@ -1,8 +1,19 @@
-/**
- * Created by harsh on 13/8/16.
- */
+/*Program to create an isoscles triangle, given n*/
+import java.util.*;
 public class Isoscles {
     public static void main(String args[]) {
-
+        int n ;
+        System.out.println("Enter n");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("");
+            for (int j = n-i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+        }
     }
 }
