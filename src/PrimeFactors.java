793b22c1e97efc54a02ac2b3b3242227f6f42950
Harsh <harshskulshrestha@gmail.com>
2016-08-16 00:20:35 +0530

    PrimeFactors complete

---
+++ src/PrimeFactors.java
@@ -0,0 +1,37 @@
+/**
+ * Created by harsh on 15/8/16.
+ */
+import java.util.*;
+public class PrimeFactors {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number");
+        n=sc.nextInt();
+        PrimeFactors obj=new PrimeFactors();
+        ArrayList ar=obj.generate(n);
+        for(int i=0;i<ar.size();i++)
+        {
+            System.out.println(ar.get(i));
+        }
+
+    }
+    private ArrayList<Integer> generate(int n) {
+        ArrayList <Integer> ar=new ArrayList<Integer>();
+        PrimeFactors obj = new PrimeFactors();
+        for (int i = 2; i <= n / 2; i++) {
+            if ((n%i == 0) && obj.isPrime(i))
+                ar.add(i);
+        }
+        return ar;
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
